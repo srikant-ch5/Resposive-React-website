e8e6ed5527f887ecae468199f3a2814c92bc43e0
sriks123 <srksriks123@gmail.com>
2021-07-19 09:58:08 +0530

    Add Render props

---
+++ src/components/RenderProps.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+class Cat extends React.Component {
+  render() {
+    const mouse = this.props.mouse;
+    return (
+      <img
+        src="./cat.jpg"
+        style={{ position: "absolute", left: mouse.x, right: mouse.y }}
+      />
+    );
+  }
+}
+
+class Mouse extends React.Component {
+  constructor(props) {
+    super(props);
+    this.hadnleMouseMove = this.hadnleMouseMove.bind(this);
+    this.state = { x: 0, y: 0 };
+  }
+
+  handleMouseMove(event) {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY,
+    });
+  }
+
+  render() {
+    return (
+      <div style={{ height: "100vh" }} onMouseMove={this.handleMouseMove}>
+        {this.props.render(this.state)}
+      </div>
+    );
+  }
+}
+
+class MouseTracker extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Move the Mouse Around </h1>
+        <Mouse render={(mouse) => <Cat mouse={mouse} />} />
+      </div>
+    );
+  }
+}
